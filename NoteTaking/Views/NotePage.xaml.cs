1ff0eb437e4f40fdd4cf363152b70b31c840e1bb
Martin Slezák <martinslezak03@gmail.com>
2022-11-18 21:53:26 +0100

    Update NotePage.xaml.cs

--- NoteTaking/Views/NotePage.xaml.cs
+++ NoteTaking/Views/NotePage.xaml.cs
@@ -13,7 +13,7 @@ public partial class NotePage : ContentPage
 		InitializeComponent();
 
 		string appDataPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pencotes");
-        string randomFileName = $"{Path.GetRandomFileName()}.txt";
+        	string randomFileName = $"{Path.GetRandomFileName()}.txt";
 
 		LoadNote(Path.Combine(appDataPath, randomFileName));
 	}
@@ -34,19 +34,15 @@ public partial class NotePage : ContentPage
 
 	private async void DeleteButton_Clicked(object sender, EventArgs e)
 	{
-		if (BindingContext is Models.Note note)
-		{
-			if (File.Exists(note.Filename))
-				File.Delete(note.Filename);
-		}
+		if (BindingContext is Models.Note note && File.Exists(note.Filename))
+			File.Delete(note.Filename);
 
 		await Shell.Current.GoToAsync("..");
 	}
 
 	private void TextEditor_TextChanged(object sender, TextChangedEventArgs e)
 	{
-
-        if (BindingContext is Models.Note note)
+        	if (BindingContext is Models.Note note)
 			File.WriteAllText(note.Filename, TextEditor.Text);
 	}
 }
